e5b0f1a987d18462780e51b7cf084830b6062aa6
Joel Ross <joelross@uw.edu>
2018-01-05 09:29:18 -0800

    Add exercises

---
+++ exercise-1/exercise.R
@@ -0,0 +1,46 @@
+# Exercise 1: calling built-in functions
+
+# Create a variable `my.name` that contains your name
+
+
+# Create a variable `name.length` that holds how many letters (including spaces)
+# are in your name (use the `nchar()` function)
+
+
+# Print the number of letters in your name
+
+
+# Create a variable `now.doing` that is your name followed by "is programming!" 
+# (use the `paste()` function)
+
+
+# Make the `now.doing` variable upper case
+
+
+### Bonus
+
+# Pick two of your favorite numbers (between 1 and 100) and assign them to 
+# variables `fav.1` and `fav.2`
+
+
+# Divide each number by the square root of 201 and save it the new value in the
+# original variable
+
+
+# Create a variable `raw.sum` that is the sum of the two variables. Use the 
+# `sum()` function for practice.
+
+
+# Create a variable `round.sum` that is the `raw.sum` rounded to 1 decimal place.
+# Use the `round()` function.
+
+
+# Create two new variables `round.1` and `round.2` that are your `fav.1` and 
+# `fav.2` variables rounded to 1 decimal places
+
+
+# Create a variable `sum.round` that is the sum of the rounded values
+
+
+# Which is bigger, `round.sum` or `sum.round`? (You can use the `max()` function!)
+
---
+++ exercise-3/exercise.R
@@ -0,0 +1,18 @@
+# Exercise 3: writing and executing functions
+
+# Define a function `AddThree` that takes a single argument and
+# returns a value 3 greater than that input
+
+
+# Create a variable `ten` that is the result of passing 7 to your `AddThree` 
+# function
+
+
+# Define a function `ImperialToMetric` that takes in two arguments: a number of 
+# feet and a number of inches
+# The function should return the equivalent length in meters
+
+
+# Create a variable `height.in.meters` by passing your height in imperial to the
+# `ImperialToMetric` function
+
