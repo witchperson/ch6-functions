adecf90586440c140d70b8adc9667226eb21f511
Joel Ross <joelross@uw.edu>
2018-01-05 09:34:42 -0800

    Add exercise solutions

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,46 +1,49 @@
 # Exercise 1: calling built-in functions
 
 # Create a variable `my.name` that contains your name
-
+my.name <- "Joel Ross"
 
 # Create a variable `name.length` that holds how many letters (including spaces)
 # are in your name (use the `nchar()` function)
-
+name.length <- nchar(my.name)
 
 # Print the number of letters in your name
-
+print(name.length)
 
 # Create a variable `now.doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
-
+now.doing <- paste(my.name, "is programming!")
 
 # Make the `now.doing` variable upper case
-
+toupper(now.doing)
 
 ### Bonus
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav.1` and `fav.2`
-
+fav.1 <- 12
+fav.2 <- 87
 
 # Divide each number by the square root of 201 and save it the new value in the
 # original variable
-
+fav.1 <- fav.1/sqrt(201)
+fav.2 <- fav.2/sqrt(201)
 
 # Create a variable `raw.sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
-
+raw.sum <-sum(fav.1, fav.2)
 
 # Create a variable `round.sum` that is the `raw.sum` rounded to 1 decimal place.
 # Use the `round()` function.
-
+round.sum <- round(raw.sum, 1)
 
 # Create two new variables `round.1` and `round.2` that are your `fav.1` and 
 # `fav.2` variables rounded to 1 decimal places
-
+round.1 <- round(fav.1, 1)
+round.2 <- round(fav.2, 1)
 
 # Create a variable `sum.round` that is the sum of the rounded values
-
+sum.round <- sum(round.1, round.2)
 
 # Which is bigger, `round.sum` or `sum.round`? (You can use the `max()` function!)
-
+max(sum.round, round.sum)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,31 +2,32 @@
 
 # Create a variable `lyric` that contains the text "I like to eat apples and 
 # bananas"
-
+lyric <- "I like to eat apples and bananas"
 
 # Use the `substr()` function to extract the 1st through 13th letters from the `lyric`, and store the result in a variable called `intro`
 # Use `?substr` to see more about this function
-
+intro <- substr(lyric, 1,13)
 
 # Use the `substr()` function to extract the 15th through the last letter of the 
 # `lyric`, and store the result in a variable called `fruits`
 # Hint: use `nchar()` to determine how many letters there are!
-
+fruits <- substr(lyric, 15, nchar(lyric))
 
 # Use the `gsub()` function to substitute all the "a"s in `fruits` with "ee".
 # Store the result in a variable called `fruits.e`
 # Hint: see http://www.endmemo.com/program/R/sub.php for a simpmle example (or 
 # use `?gsub`)
-
+fruits.e <- gsub('a','ee',fruits)
 
 # Use the `gsub()` function to substitute all the "a"s in `fruits` with "o".
 # Store the result in a variable called `fruits.o`
-
+fruits.o <- gsub('a','o',fruits)
 
 # Create a new variable `lyric.e` that is the `intro` combined with the new
 # `fruits.e` ending. Print out this variable
-
+lyric.e <- paste(intro, fruits.e)
+print(lyric.e)
 
 # Without making a new variable, print out the `intro` combined with the new
 # `fruits.o` ending
-
+print( paste(intro, fruits.o) )
